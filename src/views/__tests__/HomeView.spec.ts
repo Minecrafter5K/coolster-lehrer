117556e95ee742cbadce8918105c2b7e150d1721
Tristan Jockel <tristan.jockel@icloud.com>
2025-01-30 10:39:53 +0100

    test: fixed pinia in tests for real

--- src/views/__tests__/HomeView.spec.ts
+++ src/views/__tests__/HomeView.spec.ts
@@ -1,7 +1,12 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeEach } from 'vitest'
 import { mount } from '@vue/test-utils'
 import HomeView from '../HomeView.vue'
+import { createPinia, setActivePinia } from 'pinia'
 describe('HelloWorld', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
   it('renders properly', () => {
     const wrapper = mount(HomeView)
     expect(wrapper).toBeDefined()
