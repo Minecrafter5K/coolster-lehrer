faa607eec0fafcee45481cc28f15ee45bfcdecd8
bfischer <benediktfischer2008@gmail.com>
2025-01-31 10:55:05 +0100

    test: StatsView test

---
+++ src/views/__tests__/StatsView.spec.ts
@@ -0,0 +1,20 @@
+import { describe, it, expect, beforeEach, vi } from 'vitest'
+import { mount } from '@vue/test-utils'
+import HomeView from '../HomeView.vue'
+import { createPinia, setActivePinia } from 'pinia'
+import { createTestingPinia } from '@pinia/testing'
+
+describe('HelloWorld', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
+  it('renders properly', () => {
+    const wrapper = mount(HomeView, {
+      global: {
+        plugins: [createTestingPinia({createSpy: vi.fn})]
+      }
+    });
+    expect(wrapper).toBeDefined()
+  })
+})
