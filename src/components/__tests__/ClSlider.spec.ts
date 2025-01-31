7657745bfd70dab277c7dd603d78df554429e3c0
bfischer <benediktfischer2008@gmail.com>
2025-01-31 09:43:17 +0100

    test: 3 slider component tests

---
+++ src/components/__tests__/ClSlider.spec.ts
@@ -0,0 +1,28 @@
+import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import ClSlider from '../ClSlider.vue'
+
+describe('ClSlider Component', () => {
+  it('renders the slider input', () => {
+    const wrapper = mount(ClSlider)
+    const slider = wrapper.find('input[type="range"]')
+    expect(slider.exists()).toBe(true)
+  })
+
+  it('has correct initial value', () => {
+    const wrapper = mount(ClSlider)
+    const slider = wrapper.find('input[type="range"]')
+    expect((slider.element as HTMLInputElement).value).toBe('2')
+  })
+
+  it('snaps to closest snap point when changed', async () => {
+    const wrapper = mount(ClSlider)
+    const slider = wrapper.find('input[type="range"]')
+
+    await slider.setValue('3')
+    expect((slider.element as HTMLInputElement).value).toBe('3')
+
+    await slider.setValue('1.8')
+    expect((slider.element as HTMLInputElement).value).toBe('2')
+  })
+})
