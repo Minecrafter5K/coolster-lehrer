e0d629e51f23a5ec9e3c3b1b6e4b893998fe77f6
Tristan Jockel <tristan.jockel@icloud.com>
2025-02-03 09:59:50 +0100

    feat: added slider dots

--- src/components/ClSlider.vue
+++ src/components/ClSlider.vue
@@ -28,6 +28,15 @@ const snapToClosest = (): void => {
       class="slider"
       @input="snapToClosest"
     />
+    <div class="slider-stops">
+      <div class="slider-bg" />
+      <span
+        v-for="point in snapPoints"
+        :key="point"
+        class="stop"
+        :style="{ left: `${(point + 2) * 25}%` }"
+      ></span>
+    </div>
   </div>
 </template>
 
@@ -36,17 +45,21 @@ const snapToClosest = (): void => {
 .slider-container {
   width: 204px;
   margin-left: calc(calc(280px - 204px) / 2);
-  position: absolute;
   margin-top: 80px;
+  position: relative;
 }
 
 .slider {
   width: 100%;
   -webkit-appearance: none;
+  appearance: none;
   background: var(--cl-light-blue);
   height: 6px;
   border-radius: 3px;
   position: relative;
+
+  z-index: 1;
+  background: transparent;
 }
 
 .slider::-webkit-slider-thumb {
@@ -55,7 +68,51 @@ const snapToClosest = (): void => {
   height: 20px;
   background: var(--cl-blue);
   border-radius: 50%;
+  border: none;
   cursor: pointer;
   margin-top: 0;
+}
+
+.slider::-moz-range-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 20px;
+  height: 20px;
+  background: var(--cl-blue);
+  border-radius: 50%;
+  border: none;
+  cursor: pointer;
+  margin-top: 0;
+}
+
+.slider-bg {
+  height: 6px;
+  width: 100%;
+  width: calc(100% + 6px);
+  transform: translate(-3px, 0);
+
+  border-radius: 3px;
+
+  background-color: var(--cl-light-blue);
+}
+
+.slider-stops {
+  position: absolute;
+  left: 5%;
+  width: 90%;
+  height: 6px;
+  top: 36%;
+  transform: translateY(-50%);
+  pointer-events: none;
+}
+
+.stop {
+  width: 4px;
+  height: 4px;
+  background-color: var(--cl-slider-dots);
+  border-radius: 50%;
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
