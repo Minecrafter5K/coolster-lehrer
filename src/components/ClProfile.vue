ec04cb72779d96804fda867593f46fc258fef92c
bfischer <benediktfischer2008@gmail.com>
2025-02-03 09:57:01 +0100

    feat(p): stat profiles now have different colors based on the "Platz"

--- src/components/ClProfile.vue
+++ src/components/ClProfile.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
 import type { LehrerWithScore } from '@/interfaces/Lehrer.ts'
+import { computed } from 'vue'
 
 const props = defineProps<{ lehrer: LehrerWithScore }>()
 
-const rank = 'rank-' + props.lehrer.rank
+const rank = computed(() => 'rank-' + props.lehrer.rank)
 </script>
 
 <template>
   <div class="p_light_background" v-bind:class="rank">
-    <div class="p_dark">
+    <div class="p_dark" v-bind:class="rank">
       <h2>{{ props.lehrer.name }}</h2>
-      <div class="p_light_line"></div>
+      <div class="p_light_line" v-bind:class="rank"></div>
       <h3>{{ props.lehrer.rank }}. Platz</h3>
     </div>
   </div>
@@ -27,7 +28,13 @@ const rank = 'rank-' + props.lehrer.rank
   float: left;
 }
 .p_light_background.rank-1 {
-  background-color: var(--cl-blue);
+  background-color: var(--cl-l-gold);
+}
+.p_light_background.rank-2 {
+  background-color: var(--cl-l-silver);
+}
+.p_light_background.rank-3 {
+  background-color: var(--cl-l-bronze);
 }
 
 .p_dark {
@@ -40,6 +47,15 @@ const rank = 'rank-' + props.lehrer.rank
   position: absolute;
   bottom: 0;
 }
+.p_dark.rank-1 {
+  background-color: var(--cl-l-gold-dark);
+}
+.p_dark.rank-2 {
+  background-color: var(--cl-l-silver-dark);
+}
+.p_dark.rank-3 {
+  background-color: var(--cl-l-bronze-dark);
+}
 
 .p_light_line {
   position: absolute;
@@ -49,6 +65,15 @@ const rank = 'rank-' + props.lehrer.rank
 
   margin-left: calc(calc(280px - 204px) / 2);
   margin-top: 70px;
+}
+.p_light_line.rank-1 {
+  background-color: var(--cl-l-gold);
+}
+.p_light_line.rank-2 {
+  background-color: var(--cl-l-silver);
+}
+.p_light_line.rank-3 {
+  background-color: var(--cl-l-bronze);
 }
 
 h2 {
