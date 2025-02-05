3a12b95cc87dd2cdeb6349779206f065414fd2f1
Tristan Jockel <tristan.jockel@icloud.com>
2025-02-05 11:26:16 +0100

    feat: added start/end date to abstimmung

--- src/interfaces/Abstimmung.ts
+++ src/interfaces/Abstimmung.ts
@@ -1,11 +1,15 @@
 export interface Abstimmung {
   id?: number
   name: string
+  startDate: Date
+  endDate: Date
 }
 
 export interface AbstimmungDetail {
   id: number
   name: string
+  startDate: Date
+  endDate: Date
   winner: {
     id: number
     name: string
